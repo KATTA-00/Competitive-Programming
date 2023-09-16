44bd9198a9ea4a26bae4a86d2e946def82195e94
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2023-09-16 22:32:49 +0530

    sepNum.py error corrected

--- weekend_coding_challenge/weekend_coding_challenge_01/Chandula/sepNum.py
+++ weekend_coding_challenge/weekend_coding_challenge_01/Chandula/sepNum.py
@@ -1,30 +1,27 @@
 def seperateNumbers(s):
     len_s = len(s)//2 
-    count = 0
     currentValue = 0
     set = 0
     
-    for j in range(len_s):
-        count+=1
-        currentS = str(s[0:count])
-        currentValue = int(s[0:count])
+    for j in range(1,len_s+1):
+        currentValue = int(s[0:j])
+        currentS = str(s[0:j])
         while len(currentS)<len(s):
-            currentvalue += 1
+            currentValue += 1
             currentS = currentS + str(currentValue)
         if currentS == s:
-            print('YES',currentS[:count])
-        set = 1
-        break
+            print('YES',currentS[:j])
+            return
     
-    if set==0:
-        print('NO')
+    
+    print('NO')
 
 
 
 n = int(input().strip())
 
 for i in range(n):
-    s = str(input().strip())
+    s = input().strip()
     seperateNumbers(s)
 
 
