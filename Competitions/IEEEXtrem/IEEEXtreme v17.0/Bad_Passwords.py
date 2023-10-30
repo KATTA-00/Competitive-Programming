c0db62eb50acd4685d472b7710244e80bc6c9ac0
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2023-10-30 20:58:14 +0530

    Update Bad_Passwords.py

--- Competitions/IEEEXtrem/IEEEXtreme v17.0/Bad_Passwords.py
+++ Competitions/IEEEXtrem/IEEEXtreme v17.0/Bad_Passwords.py
@@ -31,3 +31,38 @@ for _ in range(m):
 
 
     # print(a[start:len(a) - end])
+
+##################################################################################################
+
+n = int(input())
+groups = []
+
+for _ in range(n):
+    s = input().strip().split(" ")[1:]
+    groups.append(s)
+
+m = int(input())
+
+def replace_words(word, groups, memo):
+    if word in memo:
+        return memo[word]
+
+    for group in groups:
+        for group_word in group:
+            if group_word in word:
+                word = word.replace(group_word, group[0])
+
+    memo[word] = word
+    return word
+
+for _ in range(m):
+    a, b = input().strip().split(" ")
+
+    memo = {}  # Memoization dictionary for word replacements
+    a_replaced = replace_words(a, groups, memo)
+    b_replaced = replace_words(b, groups, memo)
+
+    if a_replaced == b_replaced:
+        print("REJECT")
+    else:
+        print("OK")
